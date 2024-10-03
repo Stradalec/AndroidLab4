654accd9b71a9d8d7fdb245131dab83c51c267e0
Stradalec <pochtatipo6@gmail.com>
2024-10-03 14:48:46 +0700

    Added rView other file

--- app/src/main/java/com/example/androidlab4/MainActivity.kt
+++ app/src/main/java/com/example/androidlab4/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val recyclerView: RecyclerView = findViewById(R.id.rView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
     }
 }
