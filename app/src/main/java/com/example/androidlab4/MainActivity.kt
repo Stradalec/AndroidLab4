dd0a37c545c8e453d8f8fed072ac39ce39eb82dc
Stradalec <pochtatipo6@gmail.com>
2024-10-04 11:04:31 +0700

    Added working task4  (this is VERY unusual)

--- app/src/main/java/com/example/androidlab4/MainActivity.kt
+++ app/src/main/java/com/example/androidlab4/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -17,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val TAG = "Flickr cats"
         val internetButton: Button = findViewById(R.id.btnHTTP)
+        val coolerInternetButton: Button = findViewById(R.id.btnOkHTTP)
+
         internetButton.setOnClickListener{
             var url = URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
             Thread {
@@ -30,6 +38,23 @@ class MainActivity : AppCompatActivity() {
                     e.printStackTrace()
                 }
             }.start()
+        }
+        coolerInternetButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val client = OkHttpClient()
+                val request = Request.Builder()
+                    .url("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+                    .build()
+
+                try {
+                    val response: Response = client.newCall(request).execute()
+                    val responseData = response.body()?.string()
+
+                    Log.i(TAG, responseData ?: "No response")
+                } catch (e: Exception) {
+                    Log.e(TAG, "Error: ${e.message}")
+                }
+            }
         }
     }
 }
