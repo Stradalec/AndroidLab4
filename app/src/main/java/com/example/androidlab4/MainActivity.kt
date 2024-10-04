d8b0b77426f41018734272fb7bc8f0e46f47058a
Stradalec <pochtatipo6@gmail.com>
2024-10-04 09:44:15 +0700

    some working task1 (i need to fix this)

--- app/src/main/java/com/example/androidlab4/MainActivity.kt
+++ app/src/main/java/com/example/androidlab4/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.androidlab4
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,8 +19,51 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val recyclerView: RecyclerView = findViewById(R.id.rView)
+        var recyclerView: RecyclerView = findViewById(R.id.rView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        var colorList:ArrayList<ColorData> = ArrayList()
+        colorList.add(ColorData("Red", "#FF0000"))
+        colorList.add(ColorData("Dark Green", "#038d00"))
+        colorList.add(ColorData("Orange", "#ff8d00"))
+        colorList.add(ColorData("Yellow", "#ffff00"))
+        colorList.add(ColorData("Gray", "#212500"))
+        colorList.add(ColorData("Blue", "#2125f0"))
+        colorList.add(ColorData("Violet", "#ff0000"))
+        colorList.add(ColorData("Black", "#000000"))
+
+        var adapter = MyAdapter(this, colorList)
+        recyclerView.adapter = adapter
     }
 }
+
+data class ColorData(
+    val colorName: String,
+    val colorHex: String
+)
+class MyAdapter(private val context: Context, private val list: ArrayList<ColorData>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        var picture: View? = null;
+        var smallTextView: TextView? = null;
+        init {
+            picture = itemView.findViewById(R.id.picture)
+            smallTextView = itemView.findViewById(R.id.text)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.rview_item,parent,false)
+        return  ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val colorData = list[position]
+        holder.smallTextView?.text = colorData.colorName
+        holder.picture?.setBackgroundColor(android.graphics.Color.parseColor(colorData.colorHex))
+    }
+}
+
